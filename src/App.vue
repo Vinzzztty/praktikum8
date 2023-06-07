8a006de0315d78de732ea1971f719e31907af7f2
Vinzzztty <kevinarnandes21@gmail.com>
2023-06-07 11:57:40 +0700

    Upload file

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+    <Navbar />
+    <br />
+    <Hero />
+    <br />
+    <middleBody />
+</template>
+
+<script>
+import Navbar from "./components/Navbar.vue";
+import Hero from "./components/Hero.vue";
+import MiddleBody from "./components/MiddleBody.vue";
+
+export default {
+    name: "App",
+    components: { Navbar, Hero, MiddleBody },
+};
+</script>
